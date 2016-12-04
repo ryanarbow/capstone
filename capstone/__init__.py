a38d3a459c39e6b94f6cbcd17013770691646a85
ryanarbow <ryanarbow@gmail.com>
2016-12-04 20:51:27 +0000

    udpated structure

--- capstone/__init__.py
+++ capstone/__init__.py
@@ -1,7 +1,12 @@
+import os
+
 from flask import Flask
 
 #create app
 app = Flask(__name__)
+#
+config_path = os.environ.get("CONFIG_PATH", "blog.config.DevelopmentConfig")
+app.config.from_object(config_path)
 
 #importing after the object has been created
 #files will make use of app object
