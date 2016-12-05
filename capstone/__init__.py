d56bc4bfea5e49e6b963d0aa007b4653dddd0a54
ryanarbow <ryanarbow@gmail.com>
2016-12-05 00:07:54 +0000

    landing-page updates

--- capstone/__init__.py
+++ capstone/__init__.py
@@ -5,7 +5,7 @@ from flask import Flask
 #create app
 app = Flask(__name__)
 #
-config_path = os.environ.get("CONFIG_PATH", "blog.config.DevelopmentConfig")
+config_path = os.environ.get("CONFIG_PATH", "capstone.config.DevelopmentConfig")
 app.config.from_object(config_path)
 
 #importing after the object has been created
