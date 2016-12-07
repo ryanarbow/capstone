557ece9b1ba8c4f80644aeb595d829ede573e1e0
ryanarbow <ryanarbow@gmail.com>
2016-12-07 05:40:19 +0000

    updated classes in database.py

--- capstone/database.py
+++ capstone/database.py
@@ -8,3 +8,27 @@ engine = create_engine(app.config["SQLALCHEMY_DATABASE_URI"])
 Base = declarative_base()
 Session = sessionmaker(bind=engine)
 session = Session()
+
+from sqlalchemy import Column, Integer, String
+
+class User(Base):
+    __tablename__ = "user"
+
+    id = Column(Integer, primary_key=True)
+    url = Column(String)
+    city = Column(String)
+    
+class Profile(Base):
+    __tablename__ = "username_profile"
+    
+    id = Column(Integer, primary_key=True)
+    price_min = Column(Integer)
+    price_max = Column(Integer)
+    rating_min = Column(Integer)
+    rating_max = Column(Integer)
+    reviews_min = Column(Integer)
+    reviews_max = Column(Integer)
+    response_time_min = Column(Integer)
+    response_time_max = Column(Integer)
+    
+Base.metadata.create_all(engine)
