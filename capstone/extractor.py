fc573c6a4f9ac5453008fcb969efaae98dbf0902
ryanarbow <ryanarbow@gmail.com>
2016-12-10 23:01:24 +0000

    add data extraction to extractor.py

--- capstone/extractor.py
+++ capstone/extractor.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 import os
 import sys
 import time
+import pandas as pd
 
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.124 Safari/537.36'}
 
@@ -72,9 +73,80 @@ class DVExtractor:
                 page+=1
 
 
-    #def data_for_profile(self, user_profile_url):
-    #    for src in os.listdir(self.data_dir):
+    def data_for_profile(self, user_profile_url):
+        #Create empty lists
+        times = []
+        fees = []
+        reviews = []
+        repeats = []
+        city = []
+        ratings = []
+        name = []
+        urls = []
+        hostids = []
+        for src in os.listdir(self.data_dir):
         # all the code in pynb in #Data extraction phase
             # returns the pandas data frame
+            filename = self.data_dir + src
+            #print('Processing: ' + filename)
+            f = open(filename, 'r')
+            data = f.read()
+            f.close()
+            soup = BeautifulSoup(data, 'lxml')
+            sitters = soup.findAll('div', {'class': 'dv-host-list-item'}) 
+            for sitter in sitters:
+                rtwrap = sitter.findAll('i', {'class': 'dv-icon dv-icon__clock'})
+                responder = sitter.findAll('li', {'class': 'dv-search-badge-item'})
+                clock = len(rtwrap)
+                if clock > 0:
+                    response_time = responder[0].text.replace('Responds in', '').strip()
+                else:
+                    response_time = 0
+                fee = sitter.findAll('span', {'class': "dv-host-list-item__price__amount"})[0].text.replace('/night', '').strip()[1:]
+                review = sitter.find('span', {'class': 'dv-host-review'})
+                if review is not None and len(review) > 0:
+                    review = review.text.replace('Reviews', '').replace('Review', '').strip()
+                else:
+                    review = 0
+                full_star = sitter.findAll('i', {'class': 'dv-icon__star'})
+                rating = len(full_star)
+                half_star = sitter.findAll('i', {'class': 'dv-icon__star-half'})
+                if len(half_star) > 0:
+                    rating += .5
+                repeat = sitter.find('span', {'class': 'dv-host-repeat'})
+                if repeat is not None and len(repeat) > 0:
+                    repeat = repeat.text.strip().replace('Repeat Guests', '').replace('Repeat Guest', '')
+                else:
+                    repeat = 0
+                town = soup.findAll('span', {'class': 'dv-hero__title__location'})[0].text.strip().split(',')[0]
+                endpoint = (sitter.get('data-href'))
+                if endpoint is not None:
+                    url = endpoint
+                host = (sitter.get('data-host-id'))
+                if host is not None:
+                    hostid = host
+                fees.append(fee)
+                reviews.append(review)
+                ratings.append(rating)
+                repeats.append(repeat)
+                city.append(town)
+                times.append(response_time)
+                urls.append(url)
+                hostids.append(hostid)
+        #Dataframe from dict of objects 
+        df = pd.DataFrame({'fee':fees,
+                           'review':reviews,
+                           'rating':ratings,
+                           'repeat':repeats,
+                           'town':city, 
+                           'response_time': times,
+                           'url':urls,
+                           'hostid':hostids})
+def test():
+    test1 = DVExtractor()
+    test1.data_for_profile(nothingyet)
 
+if __name__ == "__main__":
+    test()
 
+        
