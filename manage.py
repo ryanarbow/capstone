d798eafc4cbcf0fb3e99a9ebe983ffcff8ce7e73
ryanarbow <ryanarbow@gmail.com>
2016-12-27 03:52:25 +0000

    edits to manage.py

--- manage.py
+++ manage.py
@@ -1,7 +1,7 @@
 import os
 from flask.ext.script import Manager
-
 from capstone import app
+from capstone.database import session, User, Profile_Analysis
 
 #create instance of Manager object
 manager = Manager(app)
@@ -11,6 +11,18 @@ manager = Manager(app)
 def run():
     port = int(os.environ.get('PORT', 8080))
     app.run(host='0.0.0.0', port=port)
+    
+
+def adduser():
+    user = User(
+        url= "",
+        city=city,
+        price = price,
+        rating = rating,
+        review = review,
+        response_time = response_time
+        )
+        session.save(user)
 
 if __name__ == "__main__":
     manager.run()
