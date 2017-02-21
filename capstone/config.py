18684fb15cb21ba6a9acc5743611910260d1f31e
ryanarbow <ryanarbow@gmail.com>
2017-02-21 04:43:11 +0000

    adding tests

--- capstone/config.py
+++ capstone/config.py
@@ -2,3 +2,8 @@ import os
 class DevelopmentConfig(object):
     SQLALCHEMY_DATABASE_URI = "postgresql://ubuntu:thinkful@localhost:5432/capstone"
     DEBUG = True
+
+class TestingConfig(object):
+    SQLALCHEMY_DATABASE_URI = "postgresql://ubuntu:thinkful@localhost:5432/capstone-test"
+    DEBUG = False
+    SECRET_KEY = "Not secret"
