fc8cc99cfa3b186fa9ec25ece69f5c88d6b321b5
ryanarbow <ryanarbow@gmail.com>
2017-02-25 19:53:33 +0000

    updated add_user_analysis test

--- tests/test_views_integration.py
+++ tests/test_views_integration.py
@@ -2,10 +2,12 @@ import os
 import unittest
 from urllib.parse import urlparse
 
-# Configure your app to use the testing database
+# Configuring the app to use the testing database
 os.environ["CONFIG_PATH"] = "capstone.config.TestingConfig"
 
+import capstone
 from capstone import app
+from capstone import extractor2
 from capstone.database import Base, engine, session, Profile_Analysis, Entry
 from capstone.extractor2 import *
 
@@ -17,56 +19,64 @@ class TestViews(unittest.TestCase):
 
         # Set up the tables in the database
         Base.metadata.create_all(engine)
-       
-
+    
     def tearDown(self):
         """ Test teardown """
         session.close()
         # Remove the tables and their data from the database
         Base.metadata.drop_all(engine)
-        
+       
     def test_add_user_analysis(self):
-        url = request.form['url']
-        pr_ext = extractor2.ProfileExtractor(url)
-        user_city_data = pr_ext.data_for_profile(capstone.DVExtractor)[1]
-        user_profile_data = pr_ext.data_for_profile(capstone.DVExtractor)[0]
-        profile_analysis = Profile_Analysis()
-        profile_analysis.price_min = (user_city_data.loc['fee_min'])
-        profile_analysis.price_mean = (user_city_data.loc['fee_mean']) 
-        profile_analysis.price_max = (user_city_data.loc['fee_max'])
-        profile_analysis.rating_min = (user_city_data.loc['rating_min'])
-        profile_analysis.rating_mean = (user_city_data.loc['rating_mean'])
-        profile_analysis.rating_max = (user_city_data.loc['rating_max'])
-        profile_analysis.review_min = (user_city_data.loc['review_min'])
-        profile_analysis.review_mean = (user_city_data.loc['review_mean'])
-        profile_analysis.review_max = (user_city_data.loc['review_max'])
-        profile_analysis.response_time_min = (user_city_data.loc['response_time_min'])
-        profile_analysis.response_time_mean = (user_city_data.loc['response_time_mean'])
-        profile_analysis.response_time_max = (user_city_data.loc['response_time_max'])
-        entry = Entry()
-        entry.url = url
-        entry.city = (user_profile_data)['city']
-        entry.price = (user_profile_data)['fees']
-        entry.rating = (user_profile_data)['ratings']
-        entry.review = (user_profile_data)['reviews']
-        entry.response_time = (user_profile_data)['times']
-        session.add(entry)
-        session.add(profile_analysis)
-        session.query()
-        session.commit()
-        response = self.client.post("/")
-        
+        response = self.client.post("/", data={
+            "url": "https://dogvacay.com/best-care-in-the-west-end-dog-boarding-242304?default_service=boarding",
+            "price_min": "5",
+            "price_mean":"10",
+            "price_max": "100"
+        })
+        ### Add to test
+        #"rating_min": "5",
+        #"rating_mean": "1",
+        #"rating_max":"2",
+        #"review_min":"0",
+        #"review_mean":"1",
+        #"review_max": "2",
+        #"response_time_min, "0",
+        #"response_time_mean,"1,
+        #"response_time_max,"2",
+        #"city": "Boston",
+        #"price": "40",
+        #"rating": "5",
+        #"review": "20",
+        #"response_time":"2"
+
         self.assertEqual(response.status_code, 302)
-        self.assertEqual(urlparse(response.location).path, "/")
+        self.assertEqual(urlparse(response.location).path, "/profile")
+        entries = session.query(Entry).all()
+        profile_analysis = session.query(Profile_Analysis).all()
+        self.assertEqual(len(entries), 1)
 
         entry = entries[0]
-        self.assertEqual(entry.title, "Test Entry")
-        self.assertEqual(entry.content, "Test content")
-        self.assertEqual(entry.author, self.user)
-
-        entry = entries[0]
-        
-        
+        test_profile = profile_analysis[0]
+        self.assertEqual(entry.url, "https://dogvacay.com/best-care-in-the-west-end-dog-boarding-242304?default_service=boarding")
+        self.assertEqual(test_profile.price_min, 5)
+        self.assertEqual(test_profile.price_mean, 10)
+        self.assertEqual(test_profile.price_max,100)
+        ###Add to test
+        #self.assertEqual(test_profile.rating_min, "0")
+        #self.assertEqual(test_profile.rating_mean, "1")
+        #self.assertEqual(test_profile.rating_max, "2")
+        #self.assertEqual(test_profile.review_min,"0)
+        #self.assertEqual(test_profile.review_mean,"1") 
+        #self.assertEqual(test_profile.review_max, "2)
+        #self.assertEqual(test_profile.response_time_min, "0")
+        #self.assertEqual(test_profile.response_time_mean,"1)
+        #self.assertEqual(test_profile.response_time_max,"2")
+        #self.assertEqual(test_entry.city, "Boston")
+        #self.assertEqual(test_entry.price, "40")
+        #self.assertEqual(test_entry.rating, "5")
+        #self.assertEqual(test_entry.review, "20")
+        #self.assertEqual(test_entry.response_time, "2")
+    
         
 if __name__ == "__main__":
     unittest.main()
