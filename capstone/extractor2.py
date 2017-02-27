d32a7bd7d9829e8601a6e84068570a76392deec2
ryanarbow <ryanarbow@gmail.com>
2017-02-27 00:09:55 +0000

    styling extractor2

--- capstone/extractor2.py
+++ capstone/extractor2.py
@@ -5,7 +5,7 @@ import sys
 import time
 import pandas as pd
 
-headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_0) \
+headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_0)\
 AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.124 Safari/537.36'}
 
 class ProfileExtractor(object):
@@ -23,13 +23,14 @@ class ProfileExtractor(object):
         #city = []
         #states = []
         #Extract fee
-        fee = soup.findAll('span', \
+        fee = soup.findAll('span', 
         {'class':'dv-selected-service-rate__price'})[0].text.strip()[1:]
         #Extract total number of reviews
         profile = soup.findAll('span', {'data-scroll-link': 'profile-reviews'})
         if profile is not None and len(profile) > 0:
             review = soup.findAll('span', \
-            {'data-scroll-link': 'profile-reviews'})[0].text.replace('Reviews', '').replace('Review', '').strip()
+            {'data-scroll-link': 'profile-reviews'})[0].text.replace('Reviews', 
+            '').replace('Review', '').strip()
         else:
             review = 0
         if 'Testimonial' in review:
@@ -37,7 +38,9 @@ class ProfileExtractor(object):
         #Extract response time
         response = soup.findAll('li', {'class': 'dv-profile-list__item'})
         if response is not None and len(response) > 0:
-            response_time = soup.findAll('li', {'class': 'dv-profile-list__item'})[1].text.replace('Responds within', '').strip()
+            response_time = soup.findAll('li', {
+                'class': 'dv-profile-list__item'})[1].text.replace(
+                    'Responds within', '').strip()
         else:
             response_time = 0
         if 'minutes' in response_time:
@@ -47,19 +50,25 @@ class ProfileExtractor(object):
         else:
             response_time = 3
         #Extract star rating
-        stars = review_stars = soup.findAll('ul', {'class': \
+        stars = review_stars = soup.findAll('ul', {'class':
         'rating dv-review-stars'})
         if stars is not None and len(stars) > 0:
-            review_stars = soup.findAll('ul', {'class': 'rating dv-review-stars'})[0]
-            full_star = review_stars.findAll('i', {'class': ' dv-icon dv-icon__star '})
+            review_stars = soup.findAll('ul', {'class':
+                'rating dv-review-stars'})[0]
+            full_star = review_stars.findAll('i', {'class':
+                ' dv-icon dv-icon__star '})
             rating = len(full_star)
-            half_star = soup.findAll('i', {'class': ' dv-icon dv-icon__star-half '})
+            half_star = soup.findAll('i', {'class':
+                ' dv-icon dv-icon__star-half '})
             if len(half_star) > 0:
                 rating += .5
         else:
             rating = 0
-        town = soup.findAll('div', {'class': 'dv-profile-booking__address'})[0].text.strip().split(',')[0]
-        state = soup.findAll('div', {'class': 'dv-profile-booking__address'})[0].text.strip().split(',')[1].strip()[:2]
+        town = soup.findAll('div', {'class':
+            'dv-profile-booking__address'})[0].text.strip().split(',')[0]
+        state = soup.findAll('div', {'class':
+            'dv-profile-booking__address'})[0].text.strip().split(',')[1]\
+            .strip()[:2]
         #fees.append(fee)
         #reviews.append(review)
         #ratings.append(rating)
@@ -74,8 +83,10 @@ class ProfileExtractor(object):
             "city":town,
             "states":state
         }
-        #df1['response_time'] = df1['response_time'].map(lambda x: 1 if 'minutes' in x else 2 if 'hour' in x else 3)
-        #df1['review'] = df1['review'].map(lambda x: 0 if 'Testimonial' in x else x)  
+        #df1['response_time'] = df1['response_time'].map(lambda x: 1 if 
+        #'minutes' in x else 2 if 'hour' in x else 3)
+        #df1['review'] = df1['review'].map(lambda x: 0 if 'Testimonial' in x 
+        #else x)  
         return (user_data, dv_ext.data_for_user(town)) 
 
 cities = [['tx', "austin"], [ 'ca' , 'los-angeles'], ['ma', 'boston']]
@@ -144,17 +155,23 @@ class DVExtractor(object):
             soup = BeautifulSoup(data, 'lxml')
             sitters = soup.findAll('div', {'class': 'dv-host-list-item'}) 
             for sitter in sitters:
-                rtwrap = sitter.findAll('i', {'class': 'dv-icon dv-icon__clock'})
-                responder = sitter.findAll('li', {'class': 'dv-search-badge-item'})
+                rtwrap = sitter.findAll('i', {'class':
+                    'dv-icon dv-icon__clock'})
+                responder = sitter.findAll('li', {'class':
+                    'dv-search-badge-item'})
                 clock = len(rtwrap)
                 if clock > 0:
-                    response_time = responder[0].text.replace('Responds in', '').strip()
+                    response_time = responder[0].text.replace('Responds in',
+                    '').strip()
                 else:
                     response_time = 0
-                fee = sitter.findAll('span', {'class': "dv-host-list-item__price__amount"})[0].text.replace('/night', '').strip()[1:]
+                fee = sitter.findAll('span', {'class':
+                    "dv-host-list-item__price__amount"})[0].text.replace\
+                    ('/night', '').strip()[1:]
                 review = sitter.find('span', {'class': 'dv-host-review'})
                 if review is not None and len(review) > 0:
-                    review = review.text.replace('Reviews', '').replace('Review', '').strip()
+                    review = review.text.replace('Reviews', '').replace\
+                    ('Review', '').strip()
                 else:
                     review = 0
                 full_star = sitter.findAll('i', {'class': 'dv-icon__star'})
@@ -162,7 +179,8 @@ class DVExtractor(object):
                 half_star = sitter.findAll('i', {'class': 'dv-icon__star-half'})
                 if len(half_star) > 0:
                     rating += .5
-                town = soup.findAll('span', {'class': 'dv-hero__title__location'})[0].text.strip().split(',')[0]
+                town = soup.findAll('span', {'class':
+                    'dv-hero__title__location'})[0].text.strip().split(',')[0]
                 fees.append(fee)
                 reviews.append(review)
                 ratings.append(rating)
@@ -178,7 +196,8 @@ class DVExtractor(object):
         df2['review'] = df2['review'].astype(str)
         df2['response_time'] = df2['response_time'].map(lambda x: 1 if \
         'minutes' in x else 2 if 'hour' in x else 3)
-        df2['review'] = df2['review'].map(lambda x: 0 if 'Testimonial' in x else x)                   
+        df2['review'] = df2['review'].map(lambda x: 0 if 'Testimonial' in x 
+        else x)                   
         df2['response_time'] = df2['response_time'].astype(int)
         df2['rating'] = df2['rating'].astype(int)
         df2['fee'] = df2['fee'].astype(int)
